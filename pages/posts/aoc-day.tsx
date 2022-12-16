374430cff6f81a380f62270b409e96362e1c5617
Fausto Tommasi <mmasito123@yahoo.com>
2022-12-16 13:25:26 -0600

    checkpoint

--- pages/posts/aoc-day.tsx
+++ pages/posts/aoc-day.tsx
@@ -12,15 +12,21 @@ const Moon = () => {
 
 
 export default function RenderDay() {
+    const { readFileSync } = require("fs");
     const router = useRouter();
     const data = router.query;
-    
+    var contents = "" 
     console.log(data)
-    
+   
+    const file = "../public/input/day" + data.day + ".txt";
+    const fileContents =  readFileSync(file,'utf8');
+    console.log(fileContents)
+
     return (
         <>
             <h1 >
-            Rendering page for Day: {data.day} 
+            Rendering page for Day: {data.day}
+            {contents} 
             </h1>
             <h2>
                 <Link href="..">
